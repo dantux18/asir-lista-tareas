361d873472498174010c751c7e0a9f7da05fbae8
daniel <dantux18@gmail.com>
2023-12-15 20:58:36 +0100

    add

---
+++ web/main.js
@@ -0,0 +1,9 @@
+{
+    let tarea = document.getElementById('tarea')
+    let boton = document.getElementById('botonAd')
+    boton.addEventListener("click", anadir)
+    function anadir(){
+        alert(tarea.value)
+    }
+    
+ }
